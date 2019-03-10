c0da33bcbb45332e6c9c86d6e7b5b478905ecec3
lemanahuel <lema.nahuel@gmail.com>
2019-03-10 18:42:52 -0300

    CHANGE, config domain whitelist

--- config/config.js
+++ config/config.js
@@ -5,6 +5,7 @@ module.exports = {
   SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
   WHITE_LIST_DOMAINS: ['http://localhost:5000', 'http://localhost:5001',
     'https://nl-static-www.herokuapp.com',
+    'http://nl-hapi-www.herokuapp.com', 'http://nl-hapi-api.herokuapp.com',
     'https://nl-hapi-www.herokuapp.com', 'https://nl-hapi-api.herokuapp.com',
     'https://nl-hapi-www-prod.herokuapp.com', 'https://nl-hapi-api-prod.herokuapp.com',
     'http://nl-hapi-www-prod.herokuapp.com', 'http://nl-hapi-api-prod.herokuapp.com']
