3d972265bd9f085843893a5965f789feaceb2185
lemanahuel <lema.nahuel@gmail.com>
2019-03-09 22:17:02 -0300

    CHANGE, naming

--- integrations/sendgrid.js
+++ integrations/sendgrid.js
@@ -9,7 +9,7 @@ module.exports = class Sendgrid {
   static send(params) {
     return new Promise((resolve, reject) => {
       return sgMail.send({
-        from: 'NL-HAPI-API <lema.nahuel+koa-api@gmail.com>',
+        from: 'NL-HAPI-API <lema.nahuel+hapi-api@gmail.com>',
         to: 'lema.nahuel@gmail.com',
         subject: `Notificacion de ${params.action} desde NL-HAPI-API`,
         html: `Se realizo la accion <strong>${params.action}</strong> en la tarea ${params.oldTask.title} ahora es <strong>${params.newTask.title}!</strong>`,
