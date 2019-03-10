8e250cc6139387f347bdde7fdfeeab8359635064
lemanahuel <lema.nahuel@gmail.com>
2019-03-10 18:23:14 -0300

    CHANGE, config domain whitelist

--- config/config.js
+++ config/config.js
@@ -3,5 +3,8 @@ module.exports = {
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/todo-list',
   CLOUDINARY_URL: process.env.CLOUDINARY_URL,
   SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
-  DOMAINS_WHITE_LIST: ['http://localhost:5000', 'http://localhost:5001', 'https://nl-hapi-api.herokuapp.com', 'https://nl-hapi-www.herokuapp.com']
+  WHITE_LIST_DOMAINS: ['http://localhost:5000', 'http://localhost:5001',
+    'https://nl-static-www.herokuapp.com',
+    'https://nl-hapi-www.herokuapp.com', 'https://nl-hapi-api.herokuapp.com',
+    'https://nl-hapi-www-prod.herokuapp.com', 'https://nl-hapi-api-prod.herokuapp.com']
 };
